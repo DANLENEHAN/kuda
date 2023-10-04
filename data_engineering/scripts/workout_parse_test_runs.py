84d8d07f0b7d4c10ea13f53310085b32e0e9a517
Dan <daniel.lenehan.work@gmail.com>
2023-10-04 12:33:50 +0100

    Added tree parser tests

--- data_engineering/scripts/workout_parse_test_runs.py
+++ data_engineering/scripts/workout_parse_test_runs.py
@@ -20,5 +20,7 @@ workout = parse_workout_html(
     url=broken["body"][0],
 )
 
-with open("data/workout_links/parse_test.json", "w", encoding="utf-8") as f:
+with open(
+    "data/workout_links/parsed/parse_test.json", "w", encoding="utf-8"
+) as f:
     f.write(json.dumps(workout, indent=4))
